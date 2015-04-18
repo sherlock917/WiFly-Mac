5a11d2dee7e1678610c5bee2eb56409af126d4a2
SherlockZhong <sherlock.z0917@gmail.com>
2015-04-18 18:18:59 +0800

    Initial Commit

---
+++ WiFly-Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WiFly-Mac
+//
+//  Created by Sherlock Zhong on 4/18/15.
+//  Copyright (c) 2015 SherlockZhong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WiFly-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WiFly-Mac
+//
+//  Created by Sherlock Zhong on 4/18/15.
+//  Copyright (c) 2015 SherlockZhong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
